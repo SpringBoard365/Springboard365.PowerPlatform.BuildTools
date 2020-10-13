893b4a288cd2f9529dac43df4b5eec6163037f31
David Small <dave@dsmall.co.uk>
2020-10-13 14:09:22 +0100

    feat: Power Platform Build Tools initial check in.

---
+++ Springboard365.PowerPlatform.BuildTools/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace Springboard365.PowerPlatform.BuildTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
---
+++ Springboard365.PowerPlatform.BuildTools/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Springboard365.PowerPlatform.BuildTools")]
+[assembly: AssemblyDescription("Build tools for the Power Platform.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Springboard 365 Ltd")]
+[assembly: AssemblyProduct("Springboard365.PowerPlatform.BuildTools")]
+[assembly: AssemblyCopyright("Copyright © Springboard 365 Ltd 2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: Guid("29fb945a-9aa3-49a5-a27e-b41fc9959b2b")]
+[assembly: AssemblyVersion("1.0.0")]
+[assembly: AssemblyFileVersion("1.0.0")]
+[assembly: AssemblyInformationalVersion("1.0.0")]
